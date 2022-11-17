f9c02e672dbd58add25baa65858e2479afbed4a4
Bryant Trinh <trinhbryant@gmail.com>
2022-11-16 19:36:23 -0800

    finished clock function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var choicesEl = document.getElementById('choices');
 var submitBtn - document.getElementById('submit');
 var startBtn = document.getElementById('start');
 
+var rightwrongEl = document.getElementById('rightwrong')
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
@@ -42,25 +43,69 @@ function getQuestion() {
   }
 }
 
-// Wrong guesses
-if (buttonEl.value !== questions[currentQuestionIndex].answer){
-  // time decreases by 15 seconds per wrong guess
-  time -= 15;
-  if (time < 0){
-    time = 0;
-  }
-
-  // update time on page/ display it.choices
-  timerEl.textContent = time;
-}
-
-
-
 
 function questionClick(event) {
   var buttonEl = event.target;
   // we do not want to click an element that is not a choice, make it do nothing
   if(!buttonEl.matches('.choice')) {
     return;
+  }
+}
+
+
+// Wrong guesses
+if (buttonEl.value !== questions[currentQuestionIndex].answer){
+  // time decreases by 15 seconds per wrong guess
+  time -= 15;
+  if (time < 0){
+    time = 0;
+  } 
+
+
+  // update time on page/ display it.choices
+  timerEl.textContent = time;
+
+
+// right or wrong feedback for user
+  rightwrongEl=setAttribute('class', 'rightwrong');
+  setTimeout(function() {
+    rightwrongEl.setAttribute('class', 'rightwrong hide');
+  }, 1000);
+
+// next question
+currentQuestionIndex++;
+
+// Have we gone through every question? Make an if statement.
+
+  if (time <=0 || currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+
+// quiz end, stop timer, show the ending screen, hide questions, show final score.choices
+
+function quizEnd() {
+
+  clearInterval(timerId);
+
+  var endScreenEl = document.getElementById('end-screen'); 
+  endScreenE1.removeAttribute('class');
+  questionsEl.setAttribute('class', 'hide')
+
+  var finalScoreEl = document.getElementById('final-score');
+  finalScoreEl.textContent = time;
+
+}
+
+// clock, update time, see if user runs out of time
+
+function clockTick() {
+  time--;
+  timerEl.textContent = time;
+  if (time<= 0) {
+    quizEnd();
   }
 }
